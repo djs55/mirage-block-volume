08cd570582a9f69622c84752788ee8bbf315aaff
Jon Ludlam <jonathan.ludlam@eu.citrix.com>
2013-11-14 17:17:33 +0000

    Pack into an Lvm library
    Signed-off-by: Jon Ludlam <jonathan.ludlam@eu.citrix.com>

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,5 +1,5 @@
 (* OASIS_START *)
-(* DO NOT EDIT (digest: 5c23622362d1c9dce79e326121b1216d) *)
+(* DO NOT EDIT (digest: d71b511394b67dbe7e0ab9d60774eb96) *)
 module OASISGettext = struct
 (* # 21 "src/oasis/OASISGettext.ml" *)
 
@@ -477,7 +477,7 @@ end
 open Ocamlbuild_plugin;;
 let package_default =
   {
-     MyOCamlbuildBase.lib_ocaml = [("mlvm", ["lib"])];
+     MyOCamlbuildBase.lib_ocaml = [("lvm", ["lib"])];
      lib_c = [];
      flags = [];
      includes = [];
