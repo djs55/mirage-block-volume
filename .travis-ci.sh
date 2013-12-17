17a75db8a555e2400be9e2dd2aa9ae2f4781b3e0
David Scott <dave.scott@eu.citrix.com>
2013-12-17 17:16:38 +0000

    Attempt to appease the gods of travis
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,5 +1,5 @@
 # OPAM packages needed to build tests.
-OPAM_PACKAGES="uuidm ounit rpc camldm kaputt cmdliner"
+OPAM_PACKAGES="uuidm ounit rpc camldm kaputt cmdliner cstruct mirage-block-unix re"
 
 
 case "$OCAML_VERSION,$OPAM_VERSION" in
