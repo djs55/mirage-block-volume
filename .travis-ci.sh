6e439577301aa711a5a4c0d566da4a8cf4b20e03
David Scott <dave.scott@eu.citrix.com>
2013-12-17 15:23:04 +0000

    Attempt to fix travis
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,5 +1,5 @@
 # OPAM packages needed to build tests.
-OPAM_PACKAGES="uuidm ounit rpc stdext camldm kaputt"
+OPAM_PACKAGES="uuidm ounit rpc camldm kaputt cmdliner"
 
 
 case "$OCAML_VERSION,$OPAM_VERSION" in
